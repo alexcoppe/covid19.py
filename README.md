707812d16f5490585dd99196eeee93ef8708e8e8
Ale <alexcoppe@gmail.com>
2020-03-26 23:39:45 +0100

    First working version

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # covid19.py
+
+
+### Licence
+
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE, version 2, December 2004.
+
+<a href="http://www.wtfpl.net/"><img
+       src="http://www.wtfpl.net/wp-content/uploads/2012/12/wtfpl-badge-1.png"
+       width="88" height="31" alt="WTFPL" />
+</a>
---
+++ covid19.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import argparse
+import os.path
+import sys
+import json
+import tabulate
+from urllib.request import Request, urlopen
+
+class bcolors:
+    OKGREEN = '\033[92m'
+    ERROR   = '\033[91m'
+    ENDC    = '\033[0m'
+
+def main():
+    parser = argparse.ArgumentParser(description="A command to track the COVID-19 patients all over the word using the NovelCOVID project")
+    parser.add_argument('-m', '--max', action='store', type=int, help="Maximum countries to shown (ordered by cases). Default: all", required=False, default = 0)
+    args = parser.parse_args()
+
+    maximum = args.max
+    n = 0
+
+
+    #req = Request('https://datahub.io/core/covid-19/r/countries-aggregated.json', headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request('https://corona.lmao.ninja/countries', headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+
+    headers = ["Country", "Cases", "Today Cases", "Deaths", "Today Deaths", "Recovered", "Active", "Critical", "Cases Per Million", "Deaths Per Million"]
+    
+    data = json.loads(webpage)
+    table = []
+    for country_data in data:
+        if n >= maximum:
+            break
+        n += 1
+        country = country_data.get("country")
+        cases = country_data.get("cases")
+        today_cases = country_data.get("todayCases")
+        deaths = country_data.get("deaths")
+        today_deaths = country_data.get("todayDeaths")
+        recovered = country_data.get("recovered")
+        active = country_data.get("active")
+        critical = country_data.get("critical")
+        cases_per_one_million = country_data.get("casesPerOneMillion")
+        deaths_per_one_million = country_data.get("deathsPerOneMillion")
+
+        table.append([bcolors.OKGREEN + country + bcolors.ENDC, cases, today_cases, deaths, today_deaths, recovered, active, critical, cases_per_one_million, deaths_per_one_million])
+
+    print(tabulate.tabulate(table, headers, tablefmt="grid"))
+ 
+    return 0
+
+if  __name__ == "__main__":
+    main()
+
