585c0328d7b9ceee22a7213d4f5e9e5b5bc3692c
Ale <alexcoppe@gmail.com>
2020-03-27 17:08:07 +0100

    Added part for handle the connection error

--- covid19.py
+++ covid19.py
@@ -6,6 +6,7 @@ import sys
 import json
 import tabulate
 from urllib.request import Request, urlopen
+from urllib.error import HTTPError
 
 class bcolors:
     OKGREEN = '\033[92m'
@@ -14,7 +15,7 @@ class bcolors:
 
 def main():
     parser = argparse.ArgumentParser(description="A command to track the COVID-19 patients all over the word using the NovelCOVID project")
-    parser.add_argument('-m', '--max', action='store', type=int, help="Maximum countries to shown (ordered by cases). Default: all", required=False, default = 0)
+    parser.add_argument('-m', '--max', action='store', type=int, help="Maximum countries to show (ordered by cases). Default: all", required=False, default = 0)
     args = parser.parse_args()
 
     maximum = args.max
@@ -23,14 +24,18 @@ def main():
 
     #req = Request('https://datahub.io/core/covid-19/r/countries-aggregated.json', headers={'User-Agent': 'Mozilla/5.0'})
     req = Request('https://corona.lmao.ninja/countries', headers={'User-Agent': 'Mozilla/5.0'})
-    webpage = urlopen(req).read()
+
+    try:
+        webpage = urlopen(req).read()
+    except:
+        raise SystemExit("Problem connecting to corona.lmao.ninja")
 
     headers = ["Country", "Cases", "Today Cases", "Deaths", "Today Deaths", "Recovered", "Active", "Critical", "Cases Per Million", "Deaths Per Million"]
     
     data = json.loads(webpage)
     table = []
     for country_data in data:
-        if n >= maximum:
+        if n > maximum:
             break
         n += 1
         country = country_data.get("country")
