6fc43f8fa81ef57a611ccdc9a28398a359cf65a9
Ale <alexcoppe@gmail.com>
2020-03-30 16:00:58 +0200

    Changed setup.py in order to see README.md in pypi.org

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 import setuptools
 
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setuptools.setup(
      name='covid19.py',  
-     version='0.0.2',
+     version='0.0.9',
      author="Alessandro Coppe",
      author_email="",
      description="Track the COVID-19 data in the word from the command line",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
      url="https://github.com/alexcoppe/covid19.py",
-     packages=["covid19"],
      scripts=["scripts/covid19.py"],
      install_requires=[
           'tabulate'
